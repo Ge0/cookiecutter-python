4486fbb11dc54f4c5ed5628291ff049bc6be18a4
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2017-12-09 06:34:58 +0100

    Add Python 2 classifiers

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -14,6 +14,8 @@ CLASSIFIERS = [
 {%- elif cookiecutter.project_license == 'bsd' %}
     'License :: OSI Approved :: BSD License',
 {%- endif %}
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
