582f8ed2df6a3737915f8b69c1ba86dd7f47d248
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2019-01-29 21:38:25 +0100

    Remove setuptools workaround
    See https://github.com/pypa/setuptools/pull/1180.

--- tests/rendered/travis/expected/lorem-ipsum/setup.py
+++ tests/rendered/travis/expected/lorem-ipsum/setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 
 
-setup(package_dir={"": "src"})
+setup()
--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 
 
-setup({% if cookiecutter.python_min_version < "3.0" %}package_dir={"": "src"}{% endif %})
+setup()
